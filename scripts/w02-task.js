ba56c9920ec489b7350a18323774dbdf2f3a153d
BlacIP <omotoyinbobade15@gmail.com>
2024-01-24 02:45:43 +0100

    week02

---
+++ scripts/w02-task.js
@@ -0,0 +1,50 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = 'Boluwatife Adebiyi Omotoyinbo';
+const currentYear = new Date().getFullYear();
+let profilePicture = 'images/my_image.jpeg';
+
+
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+let imageElement = document.getElementsByTagName('img')[0];
+
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+document.getElementById('year').textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+
+
+
+
+/* Step 5 - Array */
+let favFoods = ['Pizza', 'Ice Cream', 'Sushi', 'Pasta'];
+foodElement.textContent = favFoods.join(', ');
+let anotherFavFood = 'Chocolate';
+favFoods.push(anotherFavFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.textContent += '\n' + favFoods.join(', ');
+favFoods.pop();
+foodElement.textContent += '\n' + favFoods.join(', ');
+
+
+
+
+
+
+
+
